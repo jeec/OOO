7724211facd00169321282cc37c21d02f24a8b4b
joke <yongchun9123@gmail.com>
2025-10-02 10:08:10 +0800

    Initial Commit

---
+++ OOO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OOO
+//
+//  Created by Ji Yongchun on 2025/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OOO/OOOApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOOApp.swift
+//  OOO
+//
+//  Created by Ji Yongchun on 2025/10/2.
+//
+
+import SwiftUI
+
+@main
+struct OOOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
