c46a3dddf172c2db4aa366cd063012b299a43c16
joke <yongchun9123@gmail.com>
2025-10-02 10:39:47 +0800

    add commnet

--- OOO/OOOApp.swift
+++ OOO/OOOApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct OOOApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView() // similar to android compose view
         }
     }
 }
